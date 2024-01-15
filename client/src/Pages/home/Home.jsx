c4e89ae36cdfa7b48732bcb9eb04c73b3f95304e
User <michaelm810129@gmail.com>
2024-01-15 18:09:35 +1030

    Added cards to home displaying employee data

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -1,12 +1,69 @@
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useState, useEffect } from "react";
 
+export default function Home() {
+  if (!Auth.loggedIn()) {
+    window.location.replace("/Login");
+  }
+//   const [errorText, setErrorText] = useState("");
 
+  const [employeeData, setEmployeeData] = useState([]);
 
-export default function Home () {
-    if(!Auth.loggedIn()) {
-        window.location.replace('/Login');
-    }
-    return (
-        <div>Welcome to the landing page (Home)</div>
-    )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/User", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const responseData = await response.json();
+
+        if (!response.ok) {
+        //   setErrorText(responseData.message);
+          
+          setInterval(() => {
+            // setErrorText("");
+          }, 3000);
+        } else {
+          setEmployeeData(responseData.data);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Row xs={1} md={4} className="g-4">
+        
+      {employeeData.map((employee, idx) => (
+        <Col key={idx}>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="holder.js/100px160"
+              alt="Employee Image"
+            />
+            <Card.Body>
+              <Card.Title>
+                {employee.firstName} {employee.lastName}
+              </Card.Title>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>Position: {employee.role}</ListGroup.Item>
+                <ListGroup.Item>Email: {employee.email}</ListGroup.Item>
+                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
